9d8465c0f724acc8eb44aa35778bb29b7ac02b29
Ashwin Hamal <ash@ash2.local>
2019-02-09 11:40:47 +0545

    Firs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class Communicator {
+  
+}
+
+class Client extends React.Component {
+  render() {
+    return (<div>{this.props.role}</div>)
+  }
+}
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <Client
+          role="serving"
+          ref={c => this.client1 = c} />
+        <Client
+          role="connector"
+          ref={c => this.client1 = c} />
       </div>
     );
   }
